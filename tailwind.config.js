bab633612766b6da969b9b4e1e6688080930bf8f
ditomuflih <muhammadmuflihfarhan@gmail.com>
2024-12-13 21:04:15 +0700

    Fix Tailwind and build issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
